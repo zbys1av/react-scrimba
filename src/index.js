80b6c57357bc3026a58b0b755115bb2b8d69412c
zbys1av <zbyslav13@gmail.com>
2023-10-25 20:05:27 +0300

    stall change

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ import './index.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const page = (
+    <div>
+        <h1>Title</h1>
+        <h3>Mini title</h3>
+        <ol>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+        </ol>
+    </div>
+)
 
 root.render(
-    <div className='container'>
-        <h1>123</h1>
-    </div>
+    page,
+    // JSON.stringify(page)
 )
