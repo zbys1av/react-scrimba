bc2b861e0261a69759ea872274e933a9078003b1
zbys1av <zbyslav13@gmail.com>
2023-10-28 14:14:41 +0300

    conmonents + some css

--- src/index.js
+++ src/index.js
@@ -83,38 +83,99 @@ import logo from './images/react-icon.png';
 
 // TASK 4
 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// function ReactPage(){
+//     return(
+//         <div className='container'>
+//             <div className='header'>
+//                 <nav className='header__nav'>
+//                     <img src={logo} width={50} className='header__logo'></img>
+//                     <ul className='header__list'>
+//                         <li>button 1</li>
+//                         <li>button 2</li>
+//                         <li>button 3</li>
+//                     </ul>
+//                 </nav>
+//             </div>
+//             <div className='main'>
+//                 <div className='facts'>
+//                     <h3 className='facts__title'>There should be some stupid list</h3>
+//                     <ul className='facts__list'>
+//                         <li>Element 1 'asidfasdfohanvkasmdfi'</li>
+//                         <li>Element 2 'alskdjf0iosjdfkamf'</li>
+//                         <li>Element 3 'ndkvfbnuioasgsadiofj'</li>
+//                         <li>Element 4 'dklbn;miuopsklasdjf'</li>
+//                         <li>Element 5 'aklsnvmpiouheioprkngegkjlklj asdkfj'</li>
+//                     </ul>
+//                 </div>
+//             </div>
+//             <footer className='footer'>
+//                 <span>
+//                     <a href='https://github.com/zbys1av'>zbslv</a> oct 2023
+//                 </span>
+//             </footer>
+//         </div>
+//     );
+// }
+
+// root.render(
+//     <ReactPage/>
+// )
+
+
+// TASK 5
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Header(){
+    return(
+        <header className='header'>
+            <nav className='header__nav'>
+                <img src={logo} width={50} className='header__logo'></img>
+                <ul className='header__list'>
+                    <li>button 1</li>
+                    <li>button 2</li>
+                    <li>button 3</li>
+                </ul>
+            </nav>
+            <div className='header__background'></div>
+        </header>
+    )
+}
+
+function Main(){
+    return(
+        <div className='main'>
+            <div className='facts'>
+                <h3 className='facts__title'>There should be some stupid list</h3>
+                <ul className='facts__list'>
+                    <li>Element 1 'asidfasdfohanvkasmdfi'</li>
+                    <li>Element 2 'alskdjf0iosjdfkamf'</li>
+                    <li>Element 3 'ndkvfbnuioasgsadiofj'</li>
+                    <li>Element 4 'dklbn;miuopsklasdjf'</li>
+                    <li>Element 5 'aklsnvmpiouheioprkngegkjlklj asdkfj'</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+function Footer(){
+    return(
+        <footer className='footer'>
+            <span>
+                <a href='https://github.com/zbys1av'>zbslv</a> oct 2023
+            </span>
+        </footer>
+    )
+}
 
 function ReactPage(){
     return(
         <div className='container'>
-            <div className='header'>
-                <nav className='header__nav'>
-                    <img src={logo} width={50} className='header__logo'></img>
-                    <ul className='header__list'>
-                        <li>button 1</li>
-                        <li>button 2</li>
-                        <li>button 3</li>
-                    </ul>
-                </nav>
-            </div>
-            <div className='main'>
-                <div className='facts'>
-                    <h3 className='facts__title'>There should be some stupid list</h3>
-                    <ul className='facts__list'>
-                        <li>Element 1 'asidfasdfohanvkasmdfi'</li>
-                        <li>Element 2 'alskdjf0iosjdfkamf'</li>
-                        <li>Element 3 'ndkvfbnuioasgsadiofj'</li>
-                        <li>Element 4 'dklbn;miuopsklasdjf'</li>
-                        <li>Element 5 'aklsnvmpiouheioprkngegkjlklj asdkfj'</li>
-                    </ul>
-                </div>
-            </div>
-            <footer className='footer'>
-                <span>
-                    <a href='https://github.com/zbys1av'>zbslv</a> oct 2023
-                </span>
-            </footer>
+            <Header/>
+            <Main/>
+            <Footer/>
         </div>
     );
 }
