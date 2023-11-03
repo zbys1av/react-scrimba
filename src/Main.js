3f41be64159695379241d7efb5e7a400c9190d92
zbys1av <zbyslav13@gmail.com>
2023-11-03 14:11:36 +0200

    main section completed

--- src/Main.js
+++ src/Main.js
@@ -5,11 +5,32 @@ function Main(){
     return(
         <div className="main">
             <div className="main__box">
-                <img className="main__image" src="https://images.pexels.com/photos/12879014/pexels-photo-12879014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
-                <img className="main__image" src="https://images.pexels.com/photos/17038848/pexels-photo-17038848/free-photo-of-river-flowing-in-green-valley.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
-                <img className="main__image main__image--horizontal" src="https://images.pexels.com/photos/5409751/pexels-photo-5409751.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
-                <img className="main__image" src="https://images.pexels.com/photos/7292745/pexels-photo-7292745.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
-                <img className="main__image" src="https://images.pexels.com/photos/3900437/pexels-photo-3900437.jpeg" width={50}></img>
+                <div className="main__images">
+                    <div className="main__images-row">
+                        <img className="main__image" src="https://images.pexels.com/photos/12879014/pexels-photo-12879014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
+                    </div>
+                    <div className="main__images-row">
+                        <img className="main__image" src="https://images.pexels.com/photos/12879014/pexels-photo-12879014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
+                        <img className="main__image" src="https://images.pexels.com/photos/17038848/pexels-photo-17038848/free-photo-of-river-flowing-in-green-valley.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
+                    </div>
+                    <div className="main__images-row">
+                        <img className="main__image" src="https://images.pexels.com/photos/7292745/pexels-photo-7292745.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
+                        <img className="main__image" src="https://images.pexels.com/photos/3900437/pexels-photo-3900437.jpeg" width={50}></img>
+                    </div>
+                    <div className="main__images-row">
+                        <img className="main__image" src="https://images.pexels.com/photos/12879014/pexels-photo-12879014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
+                        <img className="main__image" src="https://images.pexels.com/photos/7292745/pexels-photo-7292745.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
+                    </div>
+                    <div className="main__images-row">
+                        <img className="main__image" src="https://images.pexels.com/photos/17038848/pexels-photo-17038848/free-photo-of-river-flowing-in-green-valley.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={50}></img>
+                        <img className="main__image" src="https://images.pexels.com/photos/3900437/pexels-photo-3900437.jpeg" width={50}></img>
+                    </div>
+                </div>
+                <div className="main__text-section">
+                    <h3 className="main__title title">Online Experiences</h3>
+                    <p className="main__text">Join unique interactive activities led by 
+                        one-of-a-kind hosts-all without leaving home.</p>
+                </div>
             </div>
         </div>
     )
