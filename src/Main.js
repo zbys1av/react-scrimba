436b313e608953f93075154bb5257572cc8a7cef
zbys1av <zbyslav13@gmail.com>
2023-11-12 17:36:17 +0200

    random image

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,18 @@
-import React from "react"
+import React, {useState} from "react"           //IMPORTANT {useState} is made for page to update after some changes 
 import ReactDOM from 'react-dom/client';
 
+import memeBase from "./memeBase";
+
 function Main(){
+
+    const [memeUrl, newMemeUrl] = useState(memeBase.data.memes[Math.floor(Math.random()*memeBase.data.memes.length)].url);
+
+    function getImage(){
+        const memesArray = memeBase.data.memes;
+        const randomMeme = Math.floor(Math.random()*memesArray.length);
+        newMemeUrl(memesArray[randomMeme].url);
+    }
+
     return(
         <div className="main">
             <div className="main__box">
@@ -11,7 +22,17 @@ function Main(){
                     <input className="main__input" type="text">
                     </input>
                 </div>
-                <button className="main__button">Get meme. NEOW!</button>
+
+
+
+                <button onClick={getImage} className="main__button">
+                    Get meme. NEOW!
+                </button>
+                <img src={memeUrl} alr="meme" className="main__image">
+                </img>
+
+
+
             </div>
         </div>
     )
